ce168737148065f8d60ff555c83bd3d2060f0d07
Dr-Code-lab <at.doc@ya.ru>
2024-12-13 02:42:33 +0300

    bank card

---
+++ watcher.py
@@ -0,0 +1,40 @@
+import subprocess
+import sys
+import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+class RestartHandler(FileSystemEventHandler):
+    def __init__(self, script):
+        self.script = script
+        self.process = None
+        self.start_process()
+
+    def start_process(self):
+        if self.process:
+            self.process.terminate()
+        self.process = subprocess.Popen([sys.executable, self.script])
+
+    def on_modified(self, event):
+        if event.src_path.endswith('.py'):
+            print(f'File modified: {event.src_path}')
+            self.start_process()
+
+
+if __name__ == "__main__":
+    path = '.'  # Путь к директории, которую нужно отслеживать
+    script = 'bot.py'  # Замените на имя вашего скрипта
+
+    event_handler = RestartHandler(script)
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
